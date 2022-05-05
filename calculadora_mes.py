aadcbf02f1c41bc11abdec289dc0e4eaf6c0b40c
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-05 15:21:40 -0500

    [add]new code

--- calculadora_mes.py
+++ calculadora_mes.py
@@ -18,3 +18,32 @@ def calculadoraMes(lista,Nombre):
         persona["ahorro"]["mes"]= mes 
         persona["ahorro"]["valor"]=ahorro
         print(persona)
+
+        # calculadoraMes(registroDeAhorros,nombre)
+# calculadoraMes("Enero,4,3,4;Febrero,4,3;Marzo,4,3,4;Abril,4,3,4,4" ,"daniel")
+# ('daniel', {'Enero': 11, 'Febrero': 7, 'Marzo': 11, 'Abril': 15})
+# 
+
+def calculadoraMes(lista, nombre):
+    persona = {"nombre": "", "ahorro": {}}
+    
+    persona["nombre"] = nombre
+    elementosL = lista.split(";")
+    cantElemeL = len(elementosL)
+    
+    for valores in elementosL:
+        ahorro = 0
+        valorLista = valores.split(",")
+        cantElemValor = len(valorLista)
+        for posicion in range(cantElemValor):
+            if posicion == 0:
+                mes = valorLista[posicion]
+            else :
+                ahorro += int(valorLista[posicion])
+        
+        persona["ahorro"][mes] = ahorro
+    tuplaMes = tuple(persona.values())
+    
+    return tuplaMes
+
+print(calculadoraMes("Enero,4,3,4;Febrero,4,3;Marzo,4,3,4;Abril,4,3,4,4" ,"daniel"))
